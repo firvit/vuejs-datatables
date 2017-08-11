568d3049c8691d3d42e1640baa62121ce9e7e1aa
Chirmicci Victor <firvit@gmail.com>
2017-08-11 11:56:00 +0300

    test

--- src/install.js
+++ src/install.js
@@ -1,21 +1,19 @@
-;(function () {
-    import Vue from 'vue'
-    import DataTable from './components/Datatable.vue'
+import Vue from 'vue'
+import DataTable from './components/Datatable.vue'
 
-    Datatable.config = {};
-    Datatable.install = function (Vue, options) {
-        Vue.component('datatable', DataTable)
-    };
+Datatable.config = {};
+Datatable.install = function (Vue, options) {
+    Vue.component('datatable', DataTable)
+};
 
 
-    if (typeof exports == "object") {
-        module.exports = Datatable
-    } else if (typeof define == "function" && define.amd) {
-        define([], function () {
-            return Datatable
-        })
-    } else if (window.Vue) {
-        window.Datatable = Datatable
-        Vue.use(Datatable)
-    }
-})();
+if (typeof exports == "object") {
+    module.exports = Datatable
+} else if (typeof define == "function" && define.amd) {
+    define([], function () {
+        return Datatable
+    })
+} else if (window.Vue) {
+    window.Datatable = Datatable
+    Vue.use(Datatable)
+}
