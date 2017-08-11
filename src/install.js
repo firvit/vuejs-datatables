034e4dd5a490aea46ab5a63227fef806782f4b3d
Chirmicci Victor <firvit@gmail.com>
2017-08-11 11:53:47 +0300

    test

--- src/install.js
+++ src/install.js
@@ -1,5 +1,21 @@
-import DataTable from './components/Datatable.vue'
+;(function () {
+    import Vue from 'vue'
+    import DataTable from './components/Datatable.vue'
 
-Datatable.install = function (Vue, options) {
-    Vue.component('datatable', DataTable)
-};
+    Datatable.config = {};
+    Datatable.install = function (Vue, options) {
+        Vue.component('datatable', DataTable)
+    };
+
+
+    if (typeof exports == "object") {
+        module.exports = Datatable
+    } else if (typeof define == "function" && define.amd) {
+        define([], function () {
+            return Datatable
+        })
+    } else if (window.Vue) {
+        window.Datatable = Datatable
+        Vue.use(Datatable)
+    }
+})();
