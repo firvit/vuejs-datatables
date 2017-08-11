38d72a3f71bec81a9284dd46fdea46046b09d9d4
Chirmicci Victor <firvit@gmail.com>
2017-08-11 12:08:22 +0300

    test

--- src/install.js
+++ src/install.js
@@ -1,19 +1,5 @@
-import Vue from 'vue'
 import DataTable from './components/Datatable.vue'
 
-Datatable.config = {};
-Datatable.install = function (Vue, options) {
+exports.install = function (Vue, options) {
     Vue.component('datatable', DataTable)
 };
-
-
-if (typeof exports == "object") {
-    module.exports = Datatable
-} else if (typeof define == "function" && define.amd) {
-    define([], function () {
-        return Datatable
-    })
-} else if (window.Vue) {
-    window.Datatable = Datatable
-    Vue.use(Datatable)
-}
